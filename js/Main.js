27fa71db86fecc3ddb937878eb361f9a61c1ecdb
malybaryl <kacper_barylowicz@wp.pl>
2024-11-12 17:33:26 +0100

    added descritpionts and images to certificates

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,20 @@
 import Quote from "./Quote.js";
+import ArrowButton from "./ArrowButton.js";
+
 
 class Main {
     constructor() {
-        const quote = new Quote();
+
+        // Tworzymy instancję ArrowButton, aby uzyskać dostęp do metod
+        const arrowButtonInstance = new ArrowButton();
+
+        const arrowButtonsHTML = arrowButtonInstance.getAllButtons();
+        const imagesConnectedWithArrowButtons = arrowButtonInstance.getAllImages();
+
+        for (let i = 0; i < arrowButtonsHTML.length; i++) {
+            const arrowButton = new ArrowButton(arrowButtonsHTML[i], imagesConnectedWithArrowButtons[i]);
+            arrowButtonsObjects.push(arrowButton);
+        }
     }
 }
 

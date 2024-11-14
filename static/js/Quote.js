1d3fa12d2d8461f0bf6bb7fefc54ffe95041b0d4
malybaryl <kacper_barylowicz@wp.pl>
2024-11-14 21:44:29 +0100

    first working prototype
    Page is only in polish language and in HTML, added a lot of animations, projects looks better now and have nav bar

--- static/js/Quote.js
+++ static/js/Quote.js
@@ -4,6 +4,24 @@ export default class Quote {
             [
                 ["Inwestowanie w wiedzę, zawsze przynosi największe zyski.","An investment in knowledge pays the best."],
                 ["Benjamin Franklin"]
+            ],
+            [
+                ["Nie musisz być wielki by zacząć, ale musisz zacząć by być wielkim.","You don't have to be great to start, but you have to start to be great."],
+                ["Les Brown"]
+            ],
+            [
+                ["Jedyną drogą do wykonania dobrej pracy jest kochanie tego co się robi.", "The only way to do great work is to love what you do."],
+                ["Steve Jobs"]
+            ],
+            [
+                ["Błędy są dowodem na to, że próbujesz.", "Mistakes are proof that you are trying."]
+            ],
+            [
+                ["Nie ma rzeczy niemożliwych, są tylko ludzie, którzy zbyt szybko rezygnują.", "There are no impossible things, only people who give up too soon."],
+                ['Adam Ostrowski, "Brzydki, zły i szczery"']
+            ],
+            [
+                ["W informatyce zawsze uczymy się czegoś nowego, bo każda linijka kodu może nas nauczyć więcej niż cała książka.", "In computer science, we are always learning something new, because every line of code can teach us more than an entire book."]
             ]    
         ];
         this.data = [null,null];
